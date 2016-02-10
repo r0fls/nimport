702683b709eacd271bc17785565d6350f3f1ff3d
r0fls <raphael.deem@gmail.com>
2016-02-09 19:09:16 -0800

    downloads file now, needs to initialize...

--- nimport.py
+++ nimport.py
@@ -1,11 +1,15 @@
+import os
+
 VCS_LIST = ['github','bitbucket']
 
 def nimport(mod):
+    m = mod.split('.')
     try:
-        import(mod)
-    except:
-        vcs = mod.split('.')[0]
+        mod = '.'.join(m[1:])
+        exec("mod = __import__(mod)")
+    except Exception, e:
+        vcs = m[0]
+        m = '/'.join(m[1:])
+        print(e)
         if vcs in VCS_LIST:
-            subprocess.call(git, clone, https://+vcs+mod.replace('.','/'))
-
-
+            os.system('git clone https://{0}.com/{1}'.format(vcs,m))
